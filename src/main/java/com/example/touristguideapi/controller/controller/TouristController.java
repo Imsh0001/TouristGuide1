2d6ea8726a25ec2c1ac15666af497c9c3e0b63ae
natashamohamed <namo0001@stud.kea.dk>
2024-02-11 19:35:46 +0100

    hjalp med html ellers er alt rigtigt

--- src/main/java/com/example/touristguideapi/controller/controller/TouristController.java
+++ src/main/java/com/example/touristguideapi/controller/controller/TouristController.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+@RequestMapping
 @Controller
 public class TouristController {
 
